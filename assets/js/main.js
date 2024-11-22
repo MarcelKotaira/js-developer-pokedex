794905b104ff69a3d4c5d5e83cf5688eb0b49102
MarcelKotaira <80030394+MarcelKotaira@users.noreply.github.com>
2024-11-22 06:45:03 -0300

    feat: Modal Pokemon
    Implementation of the modal to display the details (not implemented) of each Pokemon, when clicking on them.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,91 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modal = document.getElementById('pokeModal');
+const closeBtn = document.getElementsByClassName('close-btn')[0];
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+const pokemonListLi = []
+
+function abrirPokeModal(pokemon) {
+    modal.style.display = 'block';
+
+    const closeBtn = document.createElement('span')
+    closeBtn.className = 'close-btn'
+    closeBtn.innerText = '←'
+    closeBtn.onclick = () => {
+        modal.style.display = 'none'
+    }
+
+    const name = document.createElement('span')
+    name.className = 'name'
+    name.innerText = pokemon.name
+    const number = document.createElement('span')
+    number.className = 'number'
+    const pokeNumber = pokemon.number
+    number.innerText = pokeNumber < 10 ? "#00" + pokeNumber : pokeNumber < 100 ? "#0" + pokeNumber : "#" + pokeNumber
+    const types = document.createElement('div')
+    types.className = 'typesInLine'
+    pokemon.types.map((type) => {
+        const typeLi = document.createElement('span')
+        typeLi.className = "type " + type
+        typeLi.innerHTML = type
+        types.appendChild(typeLi)
+    })
+    name.innerText = pokemon.name
+    const modalHeader = document.createElement('div')
+    modalHeader.className = 'modal-header'
+    modalHeader.appendChild(name)
+    modalHeader.appendChild(number)
+    modalHeader.appendChild(types)
+
+    const bodyText = document.createElement('p')
+    bodyText.innerHTML = "Conteúdo do Pokemon"
+    const img = document.createElement('img')
+    img.className = 'image_pokemon'
+    img.src = pokemon.photo
+    img.alt = pokemon.name
+    const modalBody = document.createElement('div')
+    modalBody.className = 'modal-body'
+    modalBody.appendChild(img)
+    modalBody.appendChild(bodyText)
+
+    const content = document.createElement('div')
+    content.className = `modal-content ${pokemon.type}`
+    content.appendChild(closeBtn)
+    content.appendChild(modalHeader)
+    content.appendChild(modalBody)
+
+    modal.innerHTML = ""
+    modal.appendChild(content)
+}
 
 function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+    const li = document.createElement('li')
+    li.className = "pokemon " + pokemon.type
+    li.innerHTML = `
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+            <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+        </div>
     `
+    li.addEventListener("click", () => abrirPokeModal(pokemon))
+
+    pokemonListLi.push(li)
+    return li
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        pokemons.map(poke => pokemonList.appendChild(convertPokemonToLi(poke)))
     })
 }
 
@@ -45,3 +104,10 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+// Fechar o modal se o usuário clicar fora da área do modal
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,6 +2,8 @@
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
+
+    console.log(pokeDetail)
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
